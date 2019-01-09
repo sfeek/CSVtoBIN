289b4d8dc3b854eaa26e18999f951f9f11de0b9c
sfeek <shane.feek@gmail.com>
2019-01-09 09:05:46 -0500

    Linted

--- src/CSVLib.c
+++ src/CSVLib.c
@@ -5,14 +5,14 @@
 /*  Simple and fast CSV parser that is memory efficient and handles escaped characters and quotes correctly.
     Written by Shane Feek 04/20/2016  Email: shane.feek@gmail.com
 */
-extern char **CSVParse(char *str, int *numberOfFields)
+extern char **CSVParse (char *str, int *numberOfFields)
 {
     char *newStr = NULL;
     char currentCharacter;
     char **strArray = NULL;
 
     int quote = 0;
-    int csvLength = strlen(str);
+    int csvLength = strlen (str);
     int maxFieldCount = 2; /* Start with two fields as MAX */
     int *commaPositions = NULL;
     int currentField = 0;
@@ -22,77 +22,77 @@ extern char **CSVParse(char *str, int *numberOfFields)
     int fieldLength;
 
     /* Allocate memory for the comma position array */
-    if (!(commaPositions = calloc(1,sizeof(int) * maxFieldCount)))
+    if (! (commaPositions = calloc (1, sizeof (int) * maxFieldCount)))
     {
-        printf("Error Allocating Memory!\n");
+        printf ("Error Allocating Memory!\n");
         return NULL;
     }
 
     /* Allocate memory for "cleaned up" string the same size as the original string to guarantee that it is big enough */
-    if (!(newStr = calloc(1,sizeof(char) * csvLength)))
+    if (! (newStr = calloc (1, sizeof (char) * csvLength)))
     {
-        printf("Error Allocating Memory!\n");
+        printf ("Error Allocating Memory!\n");
         return NULL;
     }
 
     /* First pass through to record the correct comma positions */
-    for(i = 0; i < csvLength; i++)
+    for (i = 0; i < csvLength; i++)
     {
         /* Get a single character and skip any control or garbage characters */
         if ((currentCharacter = str[i]) < 32) continue;
 
         /* Handle quotes, escapes and commas */
-        switch(currentCharacter)
+        switch (currentCharacter)
         {
-            /* Check for escape character not inside quotes */
-            case 92:
+        /* Check for escape character not inside quotes */
+        case 92:
+        {
+            if (quote == 0)
             {
-                if (quote == 0)
-                {
-                    /* Move ahead one character */
-                    i++;
+                /* Move ahead one character */
+                i++;
 
-                    /* Keep the next good character and move to the next good character position*/
-                    newStr[cleanStringPosition++] = str[i];
+                /* Keep the next good character and move to the next good character position*/
+                newStr[cleanStringPosition++] = str[i];
 
-                    /* Move on to the next new character */
-                    continue;
-                }
-                break;
-            }
-
-            /* Check for quote and keep track of pairs */
-            case 34:
-            {
-                /* Toggle the pair indicator */
-                quote = 1 - quote;
-                /* Skip the quote */
+                /* Move on to the next new character */
                 continue;
             }
+            break;
+        }
 
-            /* Check for comma that is NOT inside quotes */
-            case 44:
+        /* Check for quote and keep track of pairs */
+        case 34:
+        {
+            /* Toggle the pair indicator */
+            quote = 1 - quote;
+            /* Skip the quote */
+            continue;
+        }
+
+        /* Check for comma that is NOT inside quotes */
+        case 44:
+        {
+            if (quote == 0)
             {
-                if (quote == 0)
+                /* Check to see if we need to grow our comma position array */
+                if (currentField == maxFieldCount - 1)
                 {
-                    /* Check to see if we need to grow our comma position array */
-                    if (currentField == maxFieldCount-1)
+                    /* Double in size each time */
+                    maxFieldCount *= 2;
+
+                    /* Allocate more memory for the array*/
+                    if (! (commaPositions = realloc (commaPositions, sizeof (int) * maxFieldCount)))
                     {
-                        /* Double in size each time */
-                        maxFieldCount *= 2;
-
-                        /* Allocate more memory for the array*/
-                        if (!(commaPositions = realloc(commaPositions,sizeof(int) * maxFieldCount)))
-                        {
-                            printf("Error Expanding Array!\n");
-                            return NULL;
-                        }
+                        printf ("Error Expanding Array!\n");
+                        return NULL;
                     }
-
-                    /* Keep track of the comma positions and move to the next field*/
-                    commaPositions[currentField++] = cleanStringPosition;
                 }
+
+                /* Keep track of the comma positions and move to the next field*/
+                commaPositions[currentField++] = cleanStringPosition;
             }
+        }
         }
 
         /* Keep the good characters and move to the next good character position  */
@@ -109,10 +109,10 @@ extern char **CSVParse(char *str, int *numberOfFields)
     *numberOfFields = currentField;
 
     /* Allocate an array of pointers to chars, not actually allocating any strings themselves */
-    strArray = malloc(sizeof(char *) * currentField );
+    strArray = malloc (sizeof (char *) * currentField );
 
     /* Copy the strings to the new string array */
-    for(i = 0;i < currentField;i++)
+    for (i = 0; i < currentField; i++)
     {
         /* Calculate length of the current field plus the Null Terminator*/
         fieldLength = commaPositions[i] - startPosition + 1;
@@ -121,20 +121,20 @@ extern char **CSVParse(char *str, int *numberOfFields)
         newStr[commaPositions[i]] = 0;
 
         /* Allocate memory for the current field */
-        strArray[i] = calloc(1,sizeof(char) * fieldLength);
+        strArray[i] = calloc (1, sizeof (char) * fieldLength);
 
         /* Copy the string into the new array */
-        memcpy(strArray[i],newStr + startPosition,fieldLength);
+        memcpy (strArray[i], newStr + startPosition, fieldLength);
 
         /* Move our start position to the next field */
         startPosition = commaPositions[i] + 1;
     }
 
     /* Clean up the dynamic arrays */
-    free(commaPositions);
-	commaPositions=NULL;
-    free(newStr);
-	newStr=NULL;
+    free (commaPositions);
+    commaPositions = NULL;
+    free (newStr);
+    newStr = NULL;
 
     /* Return the new array back to the calling function */
     return strArray;
@@ -145,20 +145,20 @@ extern char **CSVParse(char *str, int *numberOfFields)
             Number of strings
     Return: none
 */
-extern void cleanupStrings(char **strArray, int numberOfStrings)
+extern void cleanupStrings (char **strArray, int numberOfStrings)
 {
     int i;
 
     /* Free the individual strings */
-    for(i = 0; i < numberOfStrings; i++)
+    for (i = 0; i < numberOfStrings; i++)
     {
-        free(strArray[i]);
-		strArray[i]=NULL;
+        free (strArray[i]);
+        strArray[i] = NULL;
     }
 
     /* Once the strings themselves are freed, free the actual array itself */
-    free(strArray);
-	strArray=NULL;
+    free (strArray);
+    strArray = NULL;
 }
 
 
--- src/csvtobin.c
+++ src/csvtobin.c
@@ -5,60 +5,60 @@
 
 int main (int argc, char *argv[])
 {
-	char **parsed=NULL;
-	FILE *in,*out;
-	int numberOfFields;
-	char *line=NULL;
-	int i;
-	size_t len = 0;
-	ssize_t read;
-	char *buffer=NULL;
+    char **parsed = NULL;
+    FILE *in, *out;
+    int numberOfFields;
+    char *line = NULL;
+    int i;
+    size_t len = 0;
+    ssize_t read;
+    char *buffer = NULL;
 
-	/* Open the files */
-	in = fopen (argv[1], "r");
-	out = fopen (argv[2], "wb");
+    /* Open the files */
+    in = fopen (argv[1], "r");
+    out = fopen (argv[2], "wb");
 
-	if (in == NULL)
-		exit(EXIT_FAILURE);
+    if (in == NULL)
+        exit (EXIT_FAILURE);
 
-	if (out == NULL)
-		exit(EXIT_FAILURE);
+    if (out == NULL)
+        exit (EXIT_FAILURE);
 
-	/* Read each line */
-	while ((read = getline (&line, &len, in)) != -1)
-   	{
-		/* Parse it! */
-   		if (!(parsed = CSVParse (line,&numberOfFields)))
-   		{
-       		printf ("String parsing failed!\n");
-       		return 1;
-   		}
+    /* Read each line */
+    while ((read = getline (&line, &len, in)) != -1)
+    {
+        /* Parse it! */
+        if (! (parsed = CSVParse (line, &numberOfFields)))
+        {
+            printf ("String parsing failed!\n");
+            return 1;
+        }
 
-		/* Make Space */	
-		buffer = malloc (numberOfFields);
+        /* Make Space */
+        buffer = malloc (numberOfFields);
 
-		/* Fill it! */
-   		for (i=0;i<numberOfFields;i++)
-   		{
-       		buffer[i] = (char) atoi (parsed[i]);
-   		}
+        /* Fill it! */
+        for (i = 0; i < numberOfFields; i++)
+        {
+            buffer[i] = (char) atoi (parsed[i]);
+        }
 
-		/* Write it! */
-		fwrite (buffer,numberOfFields,1,out);
+        /* Write it! */
+        fwrite (buffer, numberOfFields, 1, out);
 
-		/* Clean up after ourselves */
-		free (buffer);
-		buffer = NULL;
+        /* Clean up after ourselves */
+        free (buffer);
+        buffer = NULL;
 
-   	   	cleanupStrings (parsed,numberOfFields);
-	}
+        cleanupStrings (parsed, numberOfFields);
+    }
 
-	/* Clean up and close out */
-	free (line);
-	line=NULL;
+    /* Clean up and close out */
+    free (line);
+    line = NULL;
 
-	fclose (in);
-	fclose (out);
+    fclose (in);
+    fclose (out);
 
 
     /* And we are out of here! */
